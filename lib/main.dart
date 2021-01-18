4cdd0b4d4c4b5953bf36eb857cd6501f1d7bc601
TWDMSoftware <oaas@cdot.in>
2021-01-18 16:54:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:bidding_market/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Phone Auth Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Phone Authentication'),
+    );
+  }
+}
+
+
