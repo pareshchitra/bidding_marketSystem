80a2dda04f87f87e9a216cc1dd38885ac6d0a753
TWDMSoftware <oaas@cdot.in>
2021-03-03 16:45:31 +0530

    product display at home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:bidding_market/models/brew.dart';
+import 'package:bidding_market/models/products.dart';
 import 'package:bidding_market/screens/home/brew_list.dart';
 import 'package:bidding_market/services/auth.dart';
 import 'package:bidding_market/services/database.dart';
@@ -6,9 +7,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+class ABC {
+  var productsList = new List<Product>();
+
+  ABC() {
+    Product p1 = new Product(id: 1);
+    productsList.add(p1);
+  }
+
+}
+
 class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +43,49 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-        //body: BrewList(),
+        body: Expanded(
+          child: ListView.builder(
+            itemCount: productsList.length,
+            itemBuilder: (ctx, i) {
+              return Container(
+                margin: const EdgeInsets.only(bottom: 25),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          color: Colors.white,
+                        ),
+                        child: Image.network(
+                          "${productsList[i].image}",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "${productsList[i].owner}",
+                            style: Theme.of(context).textTheme.title,
+                          ),
+                          Text(
+                            "${productsList[i].reservePrice}",
+                          ),
+                          SizedBox(height: 15),
+                          //MyCounter(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
